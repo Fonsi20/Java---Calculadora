4fb454513444c7fae1aaf43c2fcc5d54ffdc3fb7
FonsiFernandez <fonsi20fernandez@gmail.com>
2019-02-02 16:54:30 +0100

    Versión 1 - Funcional El juego funciona correctamente pero le faltan mucho todabía. Imagenes no incluidas en el git

---
+++ Documents/NetBeansProjects/HayUnaSerpienteEnMiBota/src/hayunaserpienteenmibota/HayUnaSerpienteEnMiBota.java
@@ -0,0 +1,37 @@
+package hayunaserpienteenmibota;
+
+/**
+ * @author Alfonso Fernández Álvarez
+ */
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class HayUnaSerpienteEnMiBota extends JFrame {
+
+    public HayUnaSerpienteEnMiBota() {
+        initUI();
+    }
+
+    public void initUI() {
+        
+        add(new VideoGame());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Hay una serpiente en mi bota!");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new HayUnaSerpienteEnMiBota();
+            ex.setVisible(true);
+        });
+
+    }
+
+}
